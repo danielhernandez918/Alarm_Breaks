737e72de5586013c74583eaa53ae6202b2463766
Daniel Hernandez <danielhernandez918@gmail.com>
2022-03-27 19:11:39 -0700

    update code

--- flask_app/models/alarm.py
+++ flask_app/models/alarm.py
@@ -2,73 +2,73 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app.models import user
 
-import datetime
-import time
-from playsound import playsound
+# import datetime
+# import time
+# from playsound import playsound
 
-class Alarm:
-    db = 'alarms'
-    def __init__( self , data ):
-        self.id = data['id']
-        self.alarm_name = data['alarm_name']
-        self.hour = data['hour']
-        self.minute = data['minute']
-        self.ampm = data['ampm']
-        self.user_id = data['user_id']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+# class Alarm:
+#     db = 'alarms'
+#     def __init__( self , data ):
+#         self.id = data['id']
+#         self.alarm_name = data['alarm_name']
+#         self.hour = data['hour']
+#         self.minute = data['minute']
+#         self.ampm = data['ampm']
+#         self.user_id = data['user_id']
+#         self.created_at = data['created_at']
+#         self.updated_at = data['updated_at']
     
-    def check_alarm_input(alarm_time):
-        """Checks to see if the user has entered in a valid alarm time"""
-        if len(alarm_time) == 1: # [Hour] Format e xample only typing 22
-            if alarm_time[0] < 24 and alarm_time[0] >= 0: # under 24 because at midnight 12am because 0
-                return True
-        if len(alarm_time) == 2: # [Hour:Minute] Format example only typing 22:01
-            if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
-                alarm_time[1] < 60 and alarm_time[1] >= 0: # under 60 because at new hour 60 becomes 0
-                return True
-        elif len(alarm_time) == 3: # [Hour:Minute:Second] Format example typing in 22:01:30
-            if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
-                alarm_time[1] < 60 and alarm_time[1] >= 0 and \
-                alarm_time[2] < 60 and alarm_time[2] >= 0: # under 60 because at new minute 60 becomes 0
-                return True
-        return False
-    # Get user input for the alarm time
-    # print("Set a time for the alarm (Ex. 06:30 or 18:30:00)")
-    def alarm(data):
-        while True:
-            alarm_input = f"{data['hour']}:{data['minute']}"
-            print(alarm_input)
-            try:
-                alarm_time = [int(n) for n in alarm_input.split(":")]
-                if check_alarm_input(alarm_time):
-                    if(data['ampm'] == 'pm'):
-                        alarm_time[0] = alarm_time[0] + 12
-                # print(alarm_time) ths is just for me. but this print would show what alarm_time looks like. 10:01 would be [10, 01]
-                    break
-                else:
-                    raise ValueError
-            except ValueError:
-                print("ERROR: Enter time in HH:MM or HH:MM:SS format")
-        #Convert the alarm time from [H:M] or [H:M:S] to seconds
-        seconds_hms = [3600, 60, 1] # Number of seconds in an Hour, Minute, and Second
-        alarm_seconds = sum([a*b for a,b in zip(seconds_hms[:len(alarm_time)], alarm_time)]) #gives how many seconds in the time inputted for example 1am is 3600 seconds
-        #print(alarm_seconds) just for me. printed version of above looks like
-        # Get the current time of day in seconds
-        now = datetime.datetime.now()
-        current_time_seconds = sum([a*b for a,b in zip(seconds_hms, [now.hour, now.minute, now.second])])#gives how many seconds at current time of day
-        # Calculate the number of seconds until alarm goes off
-        time_diff_seconds = alarm_seconds - current_time_seconds
-        # If time difference is negative, set alarm for next day
-        if time_diff_seconds < 0:
-            time_diff_seconds += 86400 # number of seconds in a day
-        # Display the amount of time until the alarm goes off
-        print("Alarm set to go off in %s" % datetime.timedelta(seconds=time_diff_seconds)) #puts seconds in time difference to 00:00:00
-        # Sleep until the alarm goes off
-        time.sleep(time_diff_seconds)
-        # Time for the alarm to go off
-        print("Wake Up!")
-        playsound(r"C:/Users/danie/OneDrive/Documents/python/flask_mysql/alarms/flask_app/static/sound/alarm.mp3")
+#     def check_alarm_input(alarm_time):
+#         """Checks to see if the user has entered in a valid alarm time"""
+#         if len(alarm_time) == 1: # [Hour] Format e xample only typing 22
+#             if alarm_time[0] < 24 and alarm_time[0] >= 0: # under 24 because at midnight 12am because 0
+#                 return True
+#         if len(alarm_time) == 2: # [Hour:Minute] Format example only typing 22:01
+#             if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+#                 alarm_time[1] < 60 and alarm_time[1] >= 0: # under 60 because at new hour 60 becomes 0
+#                 return True
+#         elif len(alarm_time) == 3: # [Hour:Minute:Second] Format example typing in 22:01:30
+#             if alarm_time[0] < 24 and alarm_time[0] >= 0 and \
+#                 alarm_time[1] < 60 and alarm_time[1] >= 0 and \
+#                 alarm_time[2] < 60 and alarm_time[2] >= 0: # under 60 because at new minute 60 becomes 0
+#                 return True
+#         return False
+#     # Get user input for the alarm time
+#     # print("Set a time for the alarm (Ex. 06:30 or 18:30:00)")
+#     def alarm(data):
+#         while True:
+#             alarm_input = f"{data['hour']}:{data['minute']}"
+#             print(alarm_input)
+#             try:
+#                 alarm_time = [int(n) for n in alarm_input.split(":")]
+#                 if check_alarm_input(alarm_time):
+#                     if(data['ampm'] == 'pm'):
+#                         alarm_time[0] = alarm_time[0] + 12
+#                 # print(alarm_time) ths is just for me. but this print would show what alarm_time looks like. 10:01 would be [10, 01]
+#                     break
+#                 else:
+#                     raise ValueError
+#             except ValueError:
+#                 print("ERROR: Enter time in HH:MM or HH:MM:SS format")
+#         #Convert the alarm time from [H:M] or [H:M:S] to seconds
+#         seconds_hms = [3600, 60, 1] # Number of seconds in an Hour, Minute, and Second
+#         alarm_seconds = sum([a*b for a,b in zip(seconds_hms[:len(alarm_time)], alarm_time)]) #gives how many seconds in the time inputted for example 1am is 3600 seconds
+#         #print(alarm_seconds) just for me. printed version of above looks like
+#         # Get the current time of day in seconds
+#         now = datetime.datetime.now()
+#         current_time_seconds = sum([a*b for a,b in zip(seconds_hms, [now.hour, now.minute, now.second])])#gives how many seconds at current time of day
+#         # Calculate the number of seconds until alarm goes off
+#         time_diff_seconds = alarm_seconds - current_time_seconds
+#         # If time difference is negative, set alarm for next day
+#         if time_diff_seconds < 0:
+#             time_diff_seconds += 86400 # number of seconds in a day
+#         # Display the amount of time until the alarm goes off
+#         print("Alarm set to go off in %s" % datetime.timedelta(seconds=time_diff_seconds)) #puts seconds in time difference to 00:00:00
+#         # Sleep until the alarm goes off
+#         time.sleep(time_diff_seconds)
+#         # Time for the alarm to go off
+#         print("Wake Up!")
+#         playsound(r"C:/Users/danie/OneDrive/Documents/python/flask_mysql/alarms/flask_app/static/sound/alarm.mp3")
 
 
     @classmethod
